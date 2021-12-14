661669deee80dc142edb8dc228a4b0ad9f110b46
zalviandyr <zukronalviandy@gmail.com>
2021-12-14 20:22:19 +0700

    feat: rounded card

--- resources/views/testimoni/index.blade.php
+++ resources/views/testimoni/index.blade.php
@@ -5,7 +5,7 @@
   <div class="content-wrapper">
     <div class="row">
       <div class="col-lg-12 grid-margin stretch-card">
-        <div class="card">
+        <div class="card card-rounded shadow-lg">
           <div class="card-body">
             <div class="d-flex justify-content-between align-items-center">
               <h4 class="card-title card-title-dash">Testimoni</h4>
